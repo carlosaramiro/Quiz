4a2c4b71e7d5880ea0c989f5364af182101b4553
carlos.aramiro <carlos.aramiro@hotmail.com>
2016-05-24 18:25:52 +0200

    Tema 8

---
+++ models/quiz.js
@@ -0,0 +1,11 @@
+// Definicion del modelo Quiz:
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Quiz',
+                          { question: DataTypes.STRING,
+                            answer:   DataTypes.STRING
+                          });
+};
+
+
+
